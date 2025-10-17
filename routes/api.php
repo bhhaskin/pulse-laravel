0432becbd16900951c30ebeb93fa605186ca1ae4
Bryan Haskin <bhhaskin@bitsofsimplicity.com>
2025-10-16 18:15:23 -0700

    added the ability to change the endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Bhhaskin\Pulse\Http\Controllers\PulseController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/', [PulseController::class, 'metrics'])
+Route::post(config('pulse.endpoint', 'pulse'), [PulseController::class, 'metrics'])
     ->name('pulse.index');
